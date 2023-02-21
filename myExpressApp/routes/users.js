c528c5722065d2781f0fd36f3f167ad53bc5afe5
HUBEDYBUBEDY <hubedybubedy123@gmail.com>
2023-02-21 15:35:42 +0000

    Modules installed and made edits to users.js in myExpressApp in attempt to connect to the database from the hosted app

--- myExpressApp/routes/users.js
+++ myExpressApp/routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+
+// SQL Stuff ---------------------------------------------
+const mysql = require('mysql');
+const fs = require('fs');
+
+var config =
+{
+  host: 'myfirstexpressappserver.database.windows.net',
+  user: 'adminuser',
+  password: 'Z6rocks!',
+  database: 'TestMySQL',
+  port: 3306,
+  ssl: {ca: fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}
+};
+
+var conn = new mysql.createConnection(config);
+
+// --------------------------------------------------------
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  conn.connect(
+    function (err) { 
+    if (err) {
+      res.send("!!! Cannot connect !!! Error:");
+      throw err;
+    } else {
+      res.send("Connection established.");
+      //queryDatabase();
+    }
+  });
 });
 
 module.exports = router;
