cb606d7636b345ebfea75affb6b21f21fdae3853
HUBEDYBUBEDY <hubedybubedy123@gmail.com>
2023-03-06 17:07:52 +0000

    Added index page

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('about_1', { title: 'Bubble\'N\'Sqeak | Recipe List' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Bubble\'N\'Sqeak | Sign In' });
 });
 
 module.exports = router;
