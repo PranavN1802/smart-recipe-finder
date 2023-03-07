0fed59e9bd86fca5ca5fe43d23c4ca2180272a5b
HUBEDYBUBEDY <hubedybubedy123@gmail.com>
2023-03-07 13:58:42 +0000

    Added all current pages to the app, linked up all buttons for the navbar and account drop down menu

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -3,11 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('about_1', { title: 'Bubble\'N\'Sqeak | Recipe List' });
+  res.render('about', { title: 'Bubble\'N\'Sqeak | About' });
+});
+
+router.get('/recipes', function(req, res, next) {
+  res.render('recipeList', { title: 'Bubble\'N\'Sqeak | Recipes' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Bubble\'N\'Sqeak | Register' });
+});
+
+router.get('/suggest', function(req, res, next) {
+  res.render('suggestPage', { title: 'Bubble\'N\'Sqeak | Suggest' });
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Bubble\'N\'Sqeak | Sign In' });
+  res.render('login', { title: 'Bubble\'N\'Sqeak | Login' });
 });
 
 module.exports = router;
