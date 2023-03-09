9bd4e76b422fb8262d8a7a8da696fc2632e895cd
HUBEDYBUBEDY <hubedybubedy123@gmail.com>
2023-03-09 15:29:29 +0000

    Started work on post requests from HTML

--- myExpressApp/public/functions/recipeIcons.js
+++ myExpressApp/public/functions/recipeIcons.js
@@ -1,5 +1,23 @@
 const iconDisplay = document.querySelector('.main_content');
 
+/*
+console.log("recipeIcons loaded");
+request.post('http://localhost:3000/recipes/search',
+        { json: { type: "initial" }},
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                body = body.json();
+                body.forEach(article => {
+                    const summary = '<p id="rcorners1"><b>' + article.name + '</b> - ' + article.summary + '</p>';
+                    iconDisplay.insertAdjacentHTML("beforeend", summary);
+                });
+                console.log(body);
+            }
+        }
+);
+*/
+
+
 fetch('http://localhost:3000/recipes/search')
     .then(response => response.json())
     .then(data => {
