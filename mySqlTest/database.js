8c5a0293a39a8c48b3af35ff083c0c16f057a966
Yanat Mengisha <yanatmengisha@Yanats-MacBook-Pro.local>
2023-03-21 13:35:28 +0000

    trying to fix my conflict

--- mySqlTest/database.js
+++ mySqlTest/database.js
@@ -6,7 +6,7 @@ const mysql = require( 'mysql2' );
 module.exports = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '15ty34RtE@MpR0JeCt',
-    // password: 'testdbpassword',
+    //password: '15ty34RtE@MpR0JeCt',
+    password: 'testdbpassword',
     database: 'RecipeApp'
 })
